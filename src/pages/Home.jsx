530901bb5120a6157e170bbd2c336214ff66487d
Uzmaa7 <uk9971khan@gmail.com>
2026-01-09 21:36:41 +0530

    add Home component with Video component embedded

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Video from '../components/home/Video'
+
+function Home(){
+    return(
+        <div>
+            <div className='h-screen w-screen bg-red-900'>
+                <Video/>
+            </div>
+           
+        </div>
+        
+    )
+}
+export default Home;
