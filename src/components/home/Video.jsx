d6bad7817217381f4ae6ce9cf8c4a31b2adbe76a
Uzmaa7 <uk9971khan@gmail.com>
2026-01-09 23:08:47 +0530

    resolve background video disappearing due to expired Video token

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1767974388~exp=1767977988~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=0228a8c3e36f8191a7458d284b0b4c76c9931f76217b2c756555bbfdfd04f20e&r=dXMtZWFzdDE%3D"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/video/video.mp4"></video>
     </div>
   )
 }
