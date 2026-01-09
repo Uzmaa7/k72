20c28bd62e88d0ec44ee3b53406b03772a192dbb
Uzmaa7 <uk9971khan@gmail.com>
2026-01-09 17:26:52 +0530

    Initial project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App(){
+  return(
+    <div className='text-6xl text-white'>
+      
+      Tailwind v3 Working 🚀
+    </div>
+   
+
+  )
+}
+export default App;
