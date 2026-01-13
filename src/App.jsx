15078bed848c5d7d77d2b4e4d2cc56889997b1b0
Uzmaa7 <uk9971khan@gmail.com>
2026-01-13 13:49:07 +0530

    feat: display stair transition animation whenever route changes via useLocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home' 
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
+
 
 function App(){
+
+
   return(
-    <div className='text-6xl text-white'>
-      
-      Tailwind v3 Working 🚀
+
+
+
+    <div className='text-white text-6xl'>
+
+      <Routes>
+        <Route path = '/'  element = {<Home/>} />
+        <Route path = '/agence' element = {<Agence/>} />
+        <Route path = '/projects' element = {<Projects/>} />
+      </Routes>
     </div>
    
 
--- src/components/common/Stair.jsx
+++ src/components/common/Stair.jsx
@@ -2,57 +2,73 @@ import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { useRef } from 'react';
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 
-function Stair(){
+function Stair(props) {
 
     const stairParentRef = useRef(null);
 
-    useGSAP(function(){
+    const currentPath = useLocation().pathname
+
+    const pageRef = useRef(null)
+
+
+    useGSAP(function () {
 
         const tl = gsap.timeline();
 
-        tl.to(stairParentRef.current,{
-            display:'block'
+        tl.to(stairParentRef.current, {
+            display: 'block'
         })
 
-        tl.from('.stair',{
-            height:0,
-            stagger:{
-                amount:-0.3
+        tl.from('.stair', {
+            height: 0,
+            stagger: {
+                amount: -0.3
             }
 
         })
         tl.to('.stair', {
-            y:'100%',
-            stagger:{
-                amount:-0.3
+            y: '100%',
+            stagger: {
+                amount: -0.3
             }
 
         })
 
-        tl.to(stairParentRef.current,{
-            display:'none'
+        tl.to(stairParentRef.current, {
+            display: 'none'
         })
         tl.to('.stair', {
             y: '0%',
         })
-    })
 
-    return(
+        gsap.from(pageRef.current, {
+            opacity: 0,
+            delay: 1,
+            scale: 1.2
+        })
+    }, [currentPath])
 
-        <div ref={stairParentRef} className='h-screen w-full  fixed z-20 top-0'>
-                    
-            <div  className='h-full w-full flex '>
-                <div className='stair h-full w-1/5 bg-black'></div>
-                <div className='stair h-full w-1/5 bg-black'></div>
-                <div className='stair h-full w-1/5 bg-black'></div>
-                <div className='stair h-full w-1/5 bg-black'></div>
-                <div className='stair h-full w-1/5 bg-black'></div>
+
+    return (
+        <div>
+            <div ref={stairParentRef} className='h-screen w-full fixed z-20 top-0'>
+                <div className='h-full w-full flex'>
+                    <div className='stair h-full w-1/5 bg-black'></div>
+                    <div className='stair h-full w-1/5 bg-black'></div>
+                    <div className='stair h-full w-1/5 bg-black'></div>
+                    <div className='stair h-full w-1/5 bg-black'></div>
+                    <div className='stair h-full w-1/5 bg-black'></div>
+                </div>
+            </div>
+            <div ref={pageRef}>
+                {props.children}
             </div>
         </div>
-        
     )
 }
+
 
 export default Stair;
